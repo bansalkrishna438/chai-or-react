a934aad843e7f9faa536d928dbb5efa6d31fd796
bansalkrishna438 <bansalkrishna438@gmail.com>
2025-04-23 20:13:56 +0530

    authentical done login or signup logout

---
+++ 12megablog/src/appwrite/auth.js
@@ -0,0 +1,65 @@
+import conf from "../conf/conf.js";
+import {Client, Account, ID} from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+        .setEndpoint(conf.appwriteUrl) // Your API Endpoint
+        .setProject(conf.appwriteProjectId); // Your project ID
+
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if(userAccount) {
+                // call another methode
+                return this.login({email, password})
+                
+            } else{
+                return userAccount;
+            }
+            
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}){
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error
+        }
+        return null;
+    }
+
+    async logout({}){
+        try {
+            // deleteSessions() is used to delete all the sessions
+            // deleteSession('current') to delete current session
+            return await this.account.deleteSessions()
+        } catch (error) {
+            throw error
+        }
+    }
+
+}
+
+const authService = new AuthService();
+
+
+
+export default authService
