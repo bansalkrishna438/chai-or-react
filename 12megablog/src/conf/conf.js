a0866e6ff8a2a436a8e49c13aca05dfad401b8d7
bansalkrishna438 <bansalkrishna438@gmail.com>
2025-04-23 13:51:10 +0530

    integrate my application with appwrite to achive backend activities

---
+++ 12megablog/src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+  appwriteUrl: String(import.meta.env.APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.APPWRITE_BUCKET_ID),
+};
+
+export default conf;
